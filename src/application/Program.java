72366331e265c1c0f6e90c5e340764741bce94e8
Luis Felipe <Felipesss.2014@gmail.com>
2023-06-27 18:29:29 -0300

    Delete no DataBase

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,65 +2,39 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import db.DB;
+import db.DbIntegrityException;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
-		Connection conn = DB.getConection();
+		Connection conn = null;
 		PreparedStatement st = null;
 		try {
 			conn = DB.getConection();
-
-			st = conn.prepareStatement(
-					"INSERT INTO seller" + 
-					"(Name, Email, BirthDate, BaseSalary, DepartmentId) " + 
-					"VALUES (?, ?, ?, ?, ?)"
-					, Statement.RETURN_GENERATED_KEYS);
-
-			st.setString(1, "Luis");
-			st.setString(2, "Luis@Gmail.com");
-			st.setDate(3, new java.sql.Date(sdf.parse("26/06/2023").getTime()));
-			st.setDouble(4, 3000.00);
-			st.setInt(5, 4);
-
+			
+			st = conn.prepareStatement("DELETE FROM department " +
+					"WHERE " +
+					"department.Id = ?");
+			
+			
+			st.setInt(1, 2);
+			
+			//Execute Update retorna a quantidade de linhas afetadas
 			int rw = st.executeUpdate();
-
-			if(rw > 0) {
-				ResultSet rs = st.getGeneratedKeys();
-				
-				while(rs.next()) {
-					//Indicando que ira receber os valores da primeira coluna (ID)
-					int novoId = rs.getInt(1);
-					
-					System.out.println("Usuario cadastrado, ID: " + novoId);
-				}
-			}
-			else {
-				System.out.println("Nenhuma linha alterada");
-			}
 			
-			
+			System.out.println("Feito, total de linhas afetadas: " + rw);
 		} 
-		catch (SQLException e) {
-			e.printStackTrace();
-		} 
-		catch (ParseException e) {
-			e.printStackTrace();
+		catch(SQLException e) {
+			throw new DbIntegrityException(e.getLocalizedMessage());
 		}
 		finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
 		}
-
+		
 	}
 }
